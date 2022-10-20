6abd2e6dadc3ae8122ac87fdbafe85d54a6ca848
v-rohan <rohanrao.dec11@gmail.com>
2022-10-20 12:47:53 +0530

    server typescript+typeorm+auth init

---
+++ server/src/middleware/AuthMiddleware.ts
@@ -0,0 +1,57 @@
+import { NextFunction, Response } from "express";
+import { IGetUserAuthInfoRequest } from "../types";
+
+const AdminCheck = (
+    request: IGetUserAuthInfoRequest,
+    response: Response,
+    next: NextFunction
+) => {
+    if (request.isAuthenticated() && request.user.role === "admin") {
+        next();
+    } else {
+        response.status(401).send("Unauthorized");
+    }
+};
+
+const AdminCheckAllowSafe = (
+    request: IGetUserAuthInfoRequest,
+    response: Response,
+    next: NextFunction
+) => {
+    if (
+        request.method == "GET" || request.method == "OPTIONS" ||
+        (request.isAuthenticated() && request.user.role === "admin")
+    ) {
+        next();
+    } else {
+        response.status(401).send("Unauthorized");
+    }
+};
+
+const AdminCheckAllowUnSafe = (
+    request: IGetUserAuthInfoRequest,
+    response: Response,
+    next: NextFunction
+) => {
+    console.log(request.method, request.isAuthenticated(), request.user);
+    if (
+        request.method == "POST" || request.method == "PUT" || request.method == "OPTIONS" ||
+        (request.isAuthenticated() && request.user.role === "admin")
+    ) {
+        next();
+    } else {
+        response.status(401).send("Unauthorized");
+    }
+};
+
+const IsAuthenticated = (
+    request: IGetUserAuthInfoRequest,
+    response: Response,
+    next: NextFunction
+) => {
+    request.isAuthenticated()
+        ? next()
+        : response.status(401).send("Unauthorized");
+};
+
+export { AdminCheck, AdminCheckAllowSafe, IsAuthenticated, AdminCheckAllowUnSafe };
